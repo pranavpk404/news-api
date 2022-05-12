dae87df36b2c18aeb08587add06f9cd8d27efedd
Pranav <pranavpk404@gmail.com>
2022-05-12 10:48:45 +0530

    changes to api

--- main.py
+++ main.py
@@ -39,30 +39,18 @@ def update_top_headline():
     FIRSTAPI = os.environ.get("FIRSTAPI")
     SECONDAPI = os.environ.get("SECONDAPI")
     THIRDAPI = os.environ.get("THIRDAPI")
-    FOURTHAPI = os.environ.get("FOURTHAPI")
-    FIFTHAPI = os.environ.get("FIFTHAPI")
-    SIXTHAPI = os.environ.get("SIXTHAPI")
-    SEVENTHAPI = os.environ.get("SEVENTHAPI")
 
     for category in CATEGORIES:
         for country in COUNTRIES_LANGUAGES:
             print(
                 f"Started category:{category} country:{country} at {strftime('%X %d %B %Y')} "
             )
-            if category == "general":
+            if country == "in":
                 get_news(country, category, FIRSTAPI)
-            elif category == "business":
+            elif country == "us":
                 get_news(country, category, SECONDAPI)
-            elif category == "health":
+            elif country == "gb":
                 get_news(country, category, THIRDAPI)
-            elif category == "science":
-                get_news(country, category, FOURTHAPI)
-            elif category == "sports":
-                get_news(country, category, FIFTHAPI)
-            elif category == "technology":
-                get_news(country, category, SIXTHAPI)
-            elif category == "entertainment":
-                get_news(country, category, SEVENTHAPI)
-
+           
 
 update_top_headline()
